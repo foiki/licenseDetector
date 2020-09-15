73c732fe36bc0c5a5b13308da2e7e86f2ad45f12
Ilya Kireev <ilyakireev53@mail.ru>
2020-09-16 00:19:52 +0300

    Added file with liceses headers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,45 +1,42 @@
 import java.io.File
 
-private val supportedLicenses = listOf("license", "apache", "mit", "gpl3", "bsd3", "lgpl")
-private val licensesExtensions = listOf("", "txt", "md")
+private val licensesHeadersParts = mapOf(
+    Pair(ApachePart, "Apache-2.0"),
+    Pair(MITPart, "MIT"),
+    Pair(GPL3Part, "GPL-3.0"),
+    Pair(BSD3ClausePart, "BSD-3-Clause"),
+    Pair(LGPL3Part, "LGPL-3.0"))
 
 private val projectLicenses = ArrayList<String>()
 private var mainLicenseExist = false
-
-fun getLicense(directory: File) {
-    val licenseTest = directory.readText()
-    licenseTest.split(" ").forEach {
-        when(it.toUpperCase()) {
-            "APACHE" -> projectLicenses.add("Apache-2.0")
-            "MIT" -> projectLicenses.add(it)
-            "GNU" -> projectLicenses.add("GPL-3.0")
-        }
-    }
-}
 
 fun checkLicenses(directory: File) {
     if (directory.length() == 0L) {
         println("There are no files in this directory!")
     }
-    var counter = 0
-    directory.walkTopDown().forEach {
-        val name = it.nameWithoutExtension
-        if (it.isFile
-            && supportedLicenses.contains(name.toLowerCase())
-            && licensesExtensions.contains(it.extension.toLowerCase())) {
-            if (!mainLicenseExist && it.parentFile == directory) {
-                mainLicenseExist = true
+    directory.walkTopDown().forEach { file ->
+        if (file.isFile && !file.isHidden && file.canRead()) {
+            val fileText = file.readText().replace("[\n\t]", " ")
+            println(fileText)
+            licensesHeadersParts.forEach {
+                if (fileText.contains(it.key)) {
+                    if (!mainLicenseExist && file.parentFile == directory) {
+                        mainLicenseExist = true
+                    }
+                    projectLicenses.add(it.value)
+                }
             }
-            getLicense(it)
-            ++counter
         }
-    }
-    if (counter == 0) {
-        println("The most popular licenses were not found in the project!")
     }
 }
 
 fun main() {
+    val string = "Licensed under the Apache License, Version 2.0 (the \"License\");\n\n" +
+            "   you may not use this file except in compliance with the License.\n" +
+            "   You may obtain a copy of the License at\n" +
+            "\n" +
+            "       http://www.apache.org/licenses/LICENSE-2.0"
+    println(ApachePart == string.replace(Regex("[\n+( )+]"), " "))
     print("Enter project directory: ")
     checkLicenses(File(readLine()!!))
     val licenses = projectLicenses.distinct()
@@ -48,8 +45,8 @@ fun main() {
     } else {
         println("Main license not found!")
     }
-    val otherLicenses = licenses.subList(1, licenses.size)
-    if (otherLicenses.isNotEmpty()) {
+    if (licenses.size > 1) {
+        val otherLicenses = licenses.subList(1, licenses.size)
         println("Other licenses of a project: ")
         otherLicenses.forEach {
             println(it)
